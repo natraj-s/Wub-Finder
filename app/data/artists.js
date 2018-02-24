a886db0d992428130708c1d74b1c4e264acfd640
natraj-s <natraj.subramanian@gmail.com>
2018-02-23 20:37:14 -0600

    Added artists JSON data. Calculates and displays appropriate artist for user

--- app/data/artists.js
+++ app/data/artists.js
@@ -1,9 +1,105 @@
-var characters = [
+var artists = [
     {
         routeName: "bonobo",
         name: "Bonobo",
         link: "",
-        scores:  []
+        scores:  [4, 1, 3, 1, 5, 3, 3, 1, 2, 4, 2]
     },
-    
-]
+    {
+        name: "Jon Hopkins",
+        link: "",
+        scores:  [4, 1, 1, 1, 5, 3, 2, 1, 2, 4, 3]
+    },
+    {
+        routeName: "justice",
+        name: "Justice",
+        link: "",
+        scores:  [3, 2, 3, 2, 3, 3, 1, 2, 2, 1, 4]
+    },
+    {
+        routeName: "daftpunk",
+        name: "Daft Punk",
+        link: "",
+        scores:  [5, 2, 4, 2, 3, 3, 1, 1, 2, 1, 3]
+    },
+    {
+        routeName: "skrillex",
+        name: "Skrillex",
+        link: "",
+        scores:  [4, 5, 3, 2, 2, 2, 2, 1, 1, 1, 3]
+    },
+    {
+        routeName: "killthenoise",
+        name: "Kill The Noise",
+        link: "",
+        scores:  [3, 3, 1, 2, 2, 2, 2, 1, 1, 1, 3]
+    },
+    {
+        routeName: "16bit",
+        name: "16 Bit",
+        link: "",
+        scores:  [5, 5, 1, 2, 2, 1, 2, 1, 2, 1, 4]
+    },
+    {
+        routeName: "noisia",
+        name: "Noisia",
+        link: "",
+        scores:  [5, 5, 5, 2, 3, 3, 2, 1, 1, 4, 3]
+    },
+    {
+        routeName: "spor",
+        name: "Spor",
+        link: "",
+        scores:  [4, 4, 4, 2, 4, 3, 2, 1, 2, 1, 4]
+    },
+    {
+        routeName: "comtruise",
+        name: "Com Truise",
+        link: "",
+        scores:  [3, 1, 1, 1, 5, 3, 2, 2, 2, 3, 3]
+    },
+    {
+        routeName: "tycho",
+        name: "Tycho",
+        link: "",
+        scores:  [3, 1, 4, 1, 5, 3, 2, 1, 2, 4, 1]
+    },
+    {
+        routeName: "emancipator",
+        name: "Emancipator",
+        link: "",
+        scores:  [3, 1, 5, 1, 3, 3, 1, 2, 2, 4, 2]
+    },
+    {
+        routeName: "tipper",
+        name: "Tipper",
+        link: "",
+        scores:  [5, 1, 4, 1, 5, 3, 2, 2, 2, 4, 3]
+    },
+    {
+        routeName: "rlgrime",
+        name: "RL Grime",
+        link: "",
+        scores:  [3, 4, 2, 2, 2, 3, 2, 2, 2, 2, 4]
+    },
+    {
+        routeName: "baauer",
+        name: "Baauer",
+        link: "",
+        scores:  [4, 3, 4, 2, 4, 3, 2, 2, 2, 2, 4]
+    },
+    {
+        routeName: "camoandkrooked",
+        name: "Camo & Krooked",
+        link: "",
+        scores:  [3, 2, 3, 2, 5, 3, 2, 1, 2, 1, 3]
+    },
+    {
+        routeName: "netsky",
+        name: "Netsky",
+        link: "",
+        scores:  [3, 3, 4, 2, 3, 3, 2, 1, 2, 4, 3]
+    },
+];
+
+module.exports = artists;
